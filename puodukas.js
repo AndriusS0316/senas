0fda65eafc1e46b0a5404880c608e1a51cfda798
Andrius <andriuss0316@gmail.com>
2021-07-28 16:21:41 +0300

    20210728

---
+++ puodukas.js
@@ -0,0 +1,17 @@
+class Puodukas {
+
+    constructor() {
+        this.spalva = "baltas";
+        this.turis = 150;
+        this.kiekis = 0;
+    }
+
+}
+
+console.log(module);
+
+module.exports = {
+    Puodukas
+};
+
+console.log(module);
